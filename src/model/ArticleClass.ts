b4405d0e98fc21d728c88aec98f4adcdfab0b8a6
yfd <670056495@qq.com>
2021-02-26 02:32:15 +0800

    finish article class

---
+++ src/model/ArticleClass.ts
@@ -0,0 +1,36 @@
+// 关于文章分类的枚举
+enum ArticleCate {
+  Work = 0,
+  Life = 1,
+  Study = 2
+}
+
+class Article {
+  id: number;
+
+  cateId: ArticleCate;
+
+  title: string;
+
+  content: string;
+
+  creatTime: string;
+
+  constructor(id = -1, cateId = ArticleCate.Work, title = '', content = '') {
+    this.id = id;
+    this.cateId = cateId;
+    this.title = title;
+    this.content = content;
+    // 自定义生成文件上传时间
+    this.creatTime = this.creatTimeFn(Date.now());
+  }
+
+  // 生成创建时间
+  creatTimeFn(timeStamp: number): string {
+    const now = new Date(timeStamp);
+    const timeStr = `${now.getFullYear()}-${(now.getMonth()) + 1}-${now.getDate()}  ${now.getHours()}:${now.getMinutes()}`;
+    return timeStr;
+  }
+}
+
+export default Article;
