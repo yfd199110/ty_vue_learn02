e95dc4e79720747dcdcc827337fc55bf94460cd7
yfd <670056495@qq.com>
2021-02-25 18:11:31 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <TopNav />
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import TopNav from './components/TopNav.vue';
+
+@Component({
+  name: 'App',
+  components: {
+    TopNav,
+  },
+})
+export default class App extends Vue {}
+</script>
+
 <style lang="stylus">
+body
+  margin: 0;
+  padding: 0;
 #app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  min-height 100vh
+  background-image linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%)
 </style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>test</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'TopNav',
+})
+export default class TopNav extends Vue {}
+</script>
